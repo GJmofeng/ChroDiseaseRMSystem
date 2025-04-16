813b77ca8a6fc4fbccca759ae2cbf1441ba83f69
GJmofeng <128493988+GJmofeng@users.noreply.github.com>
2025-04-16 18:45:06 +0800

    first commit

---
+++ SysChronicDisease/src/main/java/com/gxwz/syschronicdisease/SysChronicDiseaseApplication.java
@@ -0,0 +1,13 @@
+package com.gxwz.syschronicdisease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysChronicDiseaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysChronicDiseaseApplication.class, args);
+    }
+
+}
---
+++ SysChronicDisease/src/test/java/com/gxwz/syschronicdisease/SysChronicDiseaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxwz.syschronicdisease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysChronicDiseaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
